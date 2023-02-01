430d0da8f6af789b3731053992493361548f7407
Kavinkdev <124206012+Kavinkdev@users.noreply.github.com>
2023-02-01 15:27:02 -0500

    Add files via upload

---
+++ ice cream.js
@@ -0,0 +1,9 @@
+function  ice(){
+document.getElementById("ice cream").style.height="500px";
+document.getElementById("ice cream").style.width="500px";
+}
+function  cream(){
+    document.getElementById("ice cream").style.height="300px";
+    document.getElementById("ice cream").style.width="300px";
+    }
+
